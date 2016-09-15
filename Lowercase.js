f3286d475ca0177207e6c40d61a7793b3458a160
Nicholas Alipaz <nicholas@alipaz.net>
2016-09-15 14:59:57 -0700

    Create Lowercase.js

---
+++ Lowercase.js
@@ -0,0 +1,14 @@
+Element.prototype.replaceSelection = function (replacementText) {
+  var length = this.value.length;
+  var startPos = this.selectionStart;
+  var endPos = this.selectionEnd;
+
+  this.value = this.value.substring(0, startPos) + replacementText + this.value.substring(endPos, length);
+  this.selectionStart = startPos;
+  this.selectionEnd = endPos;
+};
+Element.prototype.getSelectedText = function () {
+  return this.value.substring(this.selectionStart, this.selectionEnd);
+};
+var elem = document.activeElement;
+elem.replaceSelection(elem.getSelectedText().toLowerCase());
