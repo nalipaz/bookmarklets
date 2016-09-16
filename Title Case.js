df63bb7193981f02acf1d3e859fab6c1918f1808
Nicholas Alipaz <nicholas@alipaz.net>
2016-09-16 10:39:04 -0700

    Load to-title-case as a remote script

--- Title Case.js
+++ Title Case.js
@@ -1,32 +1,29 @@
-String.prototype.toTitleCase = function () {
-  var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|vs?\.?|via)$/i;
+(function () {
+  function callback() {
+    Element.prototype.getSelectedText = function () {
+      return this.value.substring(this.selectionStart, this.selectionEnd);
+    };
+    Element.prototype.replaceSelection = function (replacementText) {
+      var length = this.value.length;
+      var startPos = this.selectionStart;
+      var endPos = this.selectionEnd;
 
-  return this.replace(/[A-Za-z0-9\u00C0-\u00FF]+[^\s-]*/g, function (match, index, title) {
-    if (index > 0 && index + match.length !== title.length &&
-      match.search(smallWords) > -1 && title.charAt(index - 2) !== ':' &&
-      (title.charAt(index + match.length) !== '-' || title.charAt(index - 1) === '-') &&
-      title.charAt(index - 1).search(/[^\s-]/) < 0) {
-      return match.toLowerCase();
+      this.value = this.value.substring(0, startPos) + replacementText + this.value.substring(endPos, length);
+      this.selectionStart = startPos;
+      this.selectionEnd = endPos;
+    };
+    var elem = document.activeElement;
+    elem.replaceSelection(elem.getSelectedText().toTitleCase());
+  }
+  var s = document.createElement("script");
+  s.src = "https://cdn.rawgit.com/gouch/to-title-case/dd256b78e5048ed747fb19032f98663bda71867c/to-title-case.js";
+  if (s.addEventListener) {
+    s.addEventListener("load", callback, false)
+  }
+  else {
+    if (s.readyState) {
+      s.onreadystatechange = callback
     }
-
-    if (match.substr(1).search(/[A-Z]|\../) > -1) {
-      return match;
-    }
-
-    return match.charAt(0).toUpperCase() + match.substr(1);
-  });
-};
-Element.prototype.getSelectedText = function () {
-  return this.value.substring(this.selectionStart, this.selectionEnd);
-};
-Element.prototype.replaceSelection = function (replacementText) {
-  var length = this.value.length;
-  var startPos = this.selectionStart;
-  var endPos = this.selectionEnd;
-
-  this.value = this.value.substring(0, startPos) + replacementText + this.value.substring(endPos, length);
-  this.selectionStart = startPos;
-  this.selectionEnd = endPos;
-};
-var elem = document.activeElement;
-elem.replaceSelection(elem.getSelectedText().toTitleCase());
+  }
+  document.body.appendChild(s);
+})()
