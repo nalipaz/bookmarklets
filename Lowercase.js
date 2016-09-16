4506704cb6c5ee35389e57ad9e35c9ae87bc30fd
Nicholas Alipaz <nicholas@alipaz.net>
2016-09-16 12:29:40 -0700

    Load functions from external files

--- Lowercase.js
+++ Lowercase.js
@@ -1,14 +1,17 @@
-Element.prototype.replaceSelection = function (replacementText) {
-  var length = this.value.length;
-  var startPos = this.selectionStart;
-  var endPos = this.selectionEnd;
+(function () {
+  var loaderScript = document.createElement("script");
+  loaderScript.src = "https://cdn.rawgit.com/nalipaz/bookmarklets/df32bba6d34afffdd120ba268637653968fe12b0/lib/Loader.js";
+  loaderScript.addEventListener("load", callback, false);
+  document.body.appendChild(loaderScript);
 
-  this.value = this.value.substring(0, startPos) + replacementText + this.value.substring(endPos, length);
-  this.selectionStart = startPos;
-  this.selectionEnd = endPos;
-};
-Element.prototype.getSelectedText = function () {
-  return this.value.substring(this.selectionStart, this.selectionEnd);
-};
-var elem = document.activeElement;
-elem.replaceSelection(elem.getSelectedText().toLowerCase());
+  function callback() {
+    var l = new Loader();
+    l.require([
+      'https://cdn.rawgit.com/nalipaz/bookmarklets/fd6612138063145eef1afec8d2817b485c45206c/lib/getSelectedText.js',
+      'https://cdn.rawgit.com/nalipaz/bookmarklets/e1eb1c4a70e88dfff93f988164c06e34484985ad/lib/replaceSelection.js'
+    ], function () {
+      var elem = document.activeElement;
+      elem.replaceSelection(elem.getSelectedText().toLowerCase());
+    });
+  }
+})();
